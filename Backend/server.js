fd8d9f3a82ae6b5616c3b12cd6f5c2e5c09c70e0
Yash Rane <yashrane332@gmail.com>
2026-01-03 17:40:11 +0530

    Port connection typo

--- Backend/server.js
+++ Backend/server.js
@@ -16,7 +16,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api", chatRouts);
 
 app.listen(port, () => {
-  console.log(`Server is running on ${PORT}`);
+  console.log(`Server is running on ${port}`);
   connectToDB();
 });
 
